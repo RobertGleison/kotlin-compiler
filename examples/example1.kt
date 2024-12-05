3f7539699ff8af23f47c306748acdd8adc819ce7
Robert Gleison <robertgleison12@gmail.com>
2024-12-05 22:56:15 +0000

    fix: changed example1

--- examples/example1.kt
+++ examples/example1.kt
@@ -1,10 +1,14 @@
 fun main() {
     var x: Int = 42;
+    var teste: Boolean = false;
     var y: Int = 10;
     var z = readln();
+   
     if (x > y) {
+        var scopeTest: Int = 10;
         return x;
     } else {
         return y;
     }
+    scopeTest = 10;
 }
